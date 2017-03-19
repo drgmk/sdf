6c2700c5dbd451fb77bc535ddd21a9f315e9b5c4
Grant Kennedy <grantkennedy@gmail.com>
2017-03-20 05:41:23 +0800

    pmn_percentile test

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -9,3 +9,17 @@ def test_fitting_fill_colours():
     fs = ['BS_YS','MIPS24']
     m.keep_filters(fs,colour_bases=True)
     mod_fnu = sdf.model.model_fluxes( (m,), [6300,4.5,-3], [2] )
+
+def test_fitting_pmn_pc():
+    n = 100
+    prob = np.ones(n)
+    samples = np.arange(n)+1
+    pcs = np.random.randint(99,size=10)+1
+    pc = sdf.fitting.pmn_pc(prob,samples,pcs)
+    assert(np.all(pc==pcs))
+    samples=np.tile(np.arange(n)+1,(3,1))
+    pc = sdf.fitting.pmn_pc(prob,samples,pcs,axis=1)
+    assert(np.all(pc.T==np.tile(pcs,(3,1))))
+    samples=np.tile(np.arange(n)+1,(3,3,1))
+    pc = sdf.fitting.pmn_pc(prob,samples,pcs,axis=2)
+    assert(np.all(pc.T==np.tile(pcs,(3,3,1))))
