b4f77a1b8030fe1ba3642061f2ee7e066525c6f4
drgmk <grantkennedy@gmail.com>
2020-07-09 21:01:06 +0100

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,9 @@ import numpy as np
 from .context import sdf
 
 def test_utils_plot_join_line():
-    t = {'id':['a','b','c','b'],
-         'x' :[1  ,2  ,3  ,4  ],
-         'y' :[5  ,6  ,7  ,8  ]}
+    t = {'id':np.array(['a','b','c','b']),
+         'x' :np.array([1  ,2  ,3  ,4  ]),
+         'y' :np.array([5  ,6  ,7  ,8  ])}
     x,y = sdf.utils.plot_join_line(t,'id','x','y')
-    assert(np.all(np.equal(x,[[2],[4]])))
-    assert(np.all(np.equal(y,[[6],[8]])))
+    assert(np.all(np.equal(x,[2,4])))
+    assert(np.all(np.equal(y,[6,8])))
