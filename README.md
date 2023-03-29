5857474e612b91acb35e37c0d15f1955df2a6c41
Grant Kennedy <g.kennedy@warwick.ac.uk>
2023-03-29 15:34:57 +0100

    conda env and update for mysql-connector-python

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 all your seds are belong to us
 
+## Install
+
+Download/clone `sdf`
+
+Create new environment with
+
+`conda env create -f sdf.yml`
+
+Install (far outdated) `binarytree` with:
+
+`pip install https://github.com/joowani/binarytree/archive/2.0.1.zip`
+
+Then `cd` into the sdf folder (where `setup.py` is) and install with:
+
+`pip install .`
+
 ![build status](https://travis-ci.org/drgmk/sdf.svg?branch=master)
--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ setup(
     name='sdf',
     version='0.1',
     description='all your seds are belong to us',
-    url='http://github.com/drgmk/sdf',
+    url='https://github.com/drgmk/sdf',
     author='Grant M. Kennedy',
     author_email='g.kennedy@warwick.ac.uk',
     license='MIT',
     packages=['sdf'],
     classifiers=['Programming Language :: Python :: 3'],
-    install_requires = [
-        'astropy >= v2.0.0','binarytree == v2.0.1','bokeh','corner',
-        'extinction','emcee','filelock','bleach',
-        'jinja2','matplotlib','mysql-connector','numpy',
-        'pymultinest','pytest','requests','scipy'
+    install_requires=[
+        'astropy', 'bokeh', 'corner',
+        'extinction', 'emcee', 'filelock', 'bleach',
+        'jinja2', 'matplotlib', 'mysql-connector-python', 'numpy',
+        'pymultinest', 'pytest', 'requests', 'scipy'
         ],
     include_package_data=True,
-    entry_points = {
+    entry_points={
         'console_scripts': ['sdf-fit=sdf.scripts:sdf_fit',
                             'sdf-sample=sdf.scripts:sdf_sample',
                             'sdf-cleanup=sdf.scripts:sdf_cleanup']
